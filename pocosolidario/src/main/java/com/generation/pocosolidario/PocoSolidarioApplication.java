3265f6c4122b1b4193c036a7ede5fea1fc94b417
IrisFernandaAmorim <amorim.irisf@gmail.com>
2022-08-03 12:16:30 -0300

    criação da TemaModel

---
+++ pocosolidario/src/main/java/com/generation/pocosolidario/PocoSolidarioApplication.java
@@ -0,0 +1,13 @@
+package com.generation.pocosolidario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocoSolidarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocoSolidarioApplication.class, args);
+	}
+
+}
